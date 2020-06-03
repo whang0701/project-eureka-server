1d74c92fc35233f894c476d8e5783156f79f9611
wanghang <wanghang_cd@keruyun.com>
2020-06-04 02:49:12 +0800

    初始化

---
+++ src/main/java/com/wanghang/projecteurekaserver/ProjectEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.wanghang.projecteurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ProjectEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectEurekaServerApplication.class, args);
+    }
+
+}
